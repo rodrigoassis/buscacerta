aebe2d8b48e766c3224d777fdbe4b01e94a763bc
Rodrigo Assis <rodrigo.assis.fialho@gmail.com>
2012-02-14 11:08:25 -0200

    primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Buscacerta::Application.config.secret_token = 'ac75934912e9af830f6ba0064c57190c5b147108ec5232d72e33c65e450841700ad1a0dfb57415d08b9df7d7945b069ffbe20b988c2ecb83b2b18e89c1fd64f4'
